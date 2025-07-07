0e6aa944d3b95a74b40c88f4a469fbe7659aaf1f
mira-i-o <saungweme.one@gmail.com>
2025-07-07 20:20:46 +0200

    First commit

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -7,8 +7,9 @@ import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
 
-import { Users } from './collections/Users'
+
 import { Media } from './collections/Media'
+import {Users} from "@/collections/Users";
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
